0b7c2d0891e6ad3c3b5574d1975cd4fabe90db63
unknown <aafaf.ridouan.2001@gmail.com>
2023-06-12 18:31:09 +0100

    css

--- app.py
+++ app.py
@@ -5,5 +5,8 @@ from predict import show_predict_page
 
 page = st.sidebar.selectbox("Explore Or Predict", ("Predict", "Explore"))
 
+# LINK TO THE CSS FILE
+with open("style.css")as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html = True)
 
 show_predict_page()
--- predict.py
+++ predict.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 from keras.models import model_from_json
 from utils import print_class_name, class_pred
-
+import base64
 
 # load json and create model
 json_file = open('model/Moroccanmodel.json', 'r')
@@ -14,6 +14,19 @@ model.load_weights("model/Moroccanmodel.h5")
 print("Loaded model from disk")
 
 
+# def autoplay_audio(file_path: str):
+#     with open(file_path, "rb") as f:
+#         data = f.read()
+#         b64 = base64.b64encode(data).decode()
+#         md = f"""
+#             <audio controls autoplay="true">
+#             <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
+#             </audio>
+#             """
+#         st.markdown(
+#             md,
+#             unsafe_allow_html=True,
+#         )
 
 def show_predict_page():
     st.title("Moroccan Music Classifier")
@@ -22,6 +35,7 @@ def show_predict_page():
     st.write("""### upload Moroccan Music""")
 
     uploaded_file = st.file_uploader("Choose a file")
+
     if uploaded_file is not None:
       classes_x = class_pred(model, uploaded_file)
       classname = print_class_name(classes_x)
@@ -31,7 +45,7 @@ def show_predict_page():
 
       if ok:
         st.subheader(f"The predicted class is {classname}")
-
+    
 
 
 
